3ec09472f5c504149891c37c9d1c455f37a52ada
watch-raider <07waltonmi@gmail.com>
2017-03-10 23:24:07 +0000

    Create javascript-1.js

---
+++ javascript-1.js
@@ -0,0 +1,45 @@
+function greetMe(name) {
+  var today = new Date();
+  alert("Hello " + name + ", today is " + today.toDateString());
+}
+
+//2014-15
+
+$('.choice').hide();
+
+$('.seek').on('click', function() {
+   $('.choice').toggle();
+});
+
+//2013-14
+
+$('.choice1').hide();
+
+$('.seek1').on('click', function() {
+  $('.choice1').toggle();
+});
+
+//2012-13
+
+$('.choice2').hide();
+
+$('.seek2').on('click', function() {
+  $('.choice2').toggle();
+});
+
+//2011-12
+
+$('.choice3').hide();
+
+$('.seek3').on('click', function() {
+  $('.choice3').toggle();
+});
+
+//2010-11
+
+$('.choice4').hide();
+
+$('.seek4').on('click', function() {
+  $('.choice4').toggle();
+});
+
