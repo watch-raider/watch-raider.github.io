8b5b3b5d18193b0eabd40d3df8013c336e599090
watch-raider <07waltonmi@gmail.com>
2017-04-30 15:46:13 +0100

    Update javascript-1.js

--- javascript-1.js
+++ javascript-1.js
@@ -5,41 +5,46 @@ function greetMe(name) {
 
 //2014-15
 
-$('.choice').hide();
-
-$('.seek').on('click', function() {
-   $('.choice').toggle();
+$(document).ready(function() {
+  $('.choice').hide();
+  $('.seek').click(function() {
+    $('.choice').toggle();
+  });
 });
 
 //2013-14
 
-$('.choice1').hide();
-
-$('.seek1').on('click', function() {
-  $('.choice1').toggle();
+$(document).ready(function() {
+  $('.choice1').hide();
+  $('.seek1').click(function() {
+    $('.choice1').toggle();
+  });
 });
 
 //2012-13
 
-$('.choice2').hide();
-
-$('.seek2').on('click', function() {
-  $('.choice2').toggle();
+$(document).ready(function() {
+  $('.choice2').hide();
+  $('.seek2').click(function() {
+    $('.choice2).toggle();
+  });
 });
 
 //2011-12
 
-$('.choice3').hide();
-
-$('.seek3').on('click', function() {
-  $('.choice3').toggle();
+$(document).ready(function() {
+  $('.choice3').hide();
+  $('.seek3').click(function() {
+    $('.choice3').toggle();
+  });
 });
 
 //2010-11
 
-$('.choice4').hide();
-
-$('.seek4').on('click', function() {
-  $('.choice4').toggle();
+$(document).ready(function() {
+  $('.choice4').hide();
+  $('.seek4').click(function() {
+    $('.choice4').toggle();
+  });
 });
 
